19e7af47c74dbe2e4d29395ece29a79661eec9f1
Joao Vitor <joaovitorsrs7@gmail.com>
2021-08-05 22:59:49 -0300

    new selectSquare function at Sudoku class
    this function set a value in a square at board, also the board attribute was moved to the top of the class

--- src/classes/sudoku.cpp
+++ src/classes/sudoku.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 
 class Sudoku {
+  unsigned short board[9][9] = { { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
+    { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
+    { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
+    { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
+    { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+    { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
+    { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
+    { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
+    { 0, 0, 0, 0, 8, 0, 0, 7, 9 } };
+
   private:
-  char getCharacterOfLine(unsigned short indexLine)
+  unsigned short returnIndexOfCharacter(char character)
   {
     for (unsigned short index = 0; index < 9; index++) {
       if (character == linesCharacters[index]) {
@@ -57,23 +67,28 @@ class Sudoku {
     return false;
   }
 
+  bool selectSquare(char lineCharacrter, unsigned short squareIndex, unsigned short value)
+  {
+
+    unsigned short lineIndex = returnIndexOfCharacter(lineCharacrter);
+    if (!validateIfSquareIsPossible(lineIndex, squareIndex, value)) {
+      std::cout << "string" << std::endl;
+      return false;
+    }
+
+    board[lineIndex][squareIndex] = value;
+
+    return true;
+  }
+
   char linesCharacters[9] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I' };
-  unsigned short board[9][9] = { { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
-    { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
-    { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
-    { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
-    { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
-    { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
-    { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
-    { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
-    { 0, 0, 0, 0, 8, 0, 0, 7, 9 } };
 
   void displayBoard(void)
   {
-    std::cout << "  0 1 2 3 4 5 6 7 8" << std::endl;
+    std::cout << "   0 1 2 3 4 5 6 7 8" << std::endl;
     for (unsigned short indexLine = 0; indexLine < 9; indexLine++) {
-      std::cout << "  - - - - - - - - -" << std::endl;
-      std::cout << getCharacterOfLine(indexLine) << " ";
+      std::cout << "   - - - - - - - - -" << std::endl;
+      std::cout << linesCharacters[indexLine] << " ";
       for (unsigned short indexSqure = 0; indexSqure < 9; indexSqure++) {
         if (board[indexLine][indexSqure] == 0) {
           std::cout << "| ";
